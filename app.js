724d5a479e3ffe7e9e25a763b428023b54f1d74e
Akhmad Wildan <wildanakhmad3584@gmail.com>
2021-07-06 16:06:27 +0700

    berahsil menambahkan fitur tambah contact

--- app.js
+++ app.js
@@ -1,83 +1,146 @@
-// const express = require('express');
-// const app = express();
-// const expressLayouts = require('express-ejs-layouts');
-// const { body, validationResult, check } = require('express-validator');
-// const session = require('express-session');
-// const cookieParser = require("cookie-parser");
-// const flash = require('connect-flash');
-// const port = 3000;
-
-
-// // Gunakan ejs
-// app.set('view engine', 'ejs');
-
-// // Third-Party Midleware
-// app.use(expressLayouts);
-
-// // Built-in Midleware
-
-// app.use(express.static('public'));
-// app.use(express.urlencoded({extended: true}));
-
-// // Konfigurasi flash
-
-// app.use(cookieParser('secret'));
-// app.use(
-//     session({
-//         cookie: {maxAge: 6000},
-//         secret: 'secret',
-//         resave: true,
-//         saveUninitialized: true,
-//     })
-// );
-
-// app.use(flash());
-
-// app.get('/about', (req, res)=>{
-//     // res.sendFile('./about.html', {root:__dirname});
-//     res.render('about', {
-//         layout: 'layouts/main-layouts',
-//         title: 'view about'
-//     });
-// });
-// app.get('/contact', (req, res)=>{
-//     res.render('contact', {
-//         layout: 'layouts/main-layouts',
-//         title: 'halaman contact',
-//         msg: req.flash('msg'),
-//     });
-// }); 
-
-// app.get('/', (req, res)=>{
-//     const kariyawan = 
-//     [
-//         {
-//         nama: 'akhmad',
-//         nik: '234'
-//         },
-//         {
-//         nama: 'wildan',
-//         nik: '234'
-//         },
-//         {
-//         nama: 'arthur',
-//         nik: '234'
-//         }
-//     ]
-//     res.render('index', {
-//         layout: 'layouts/main-layouts',
-//         title: 'Halaman index',
-//         kariyawan,
-//     });
-// });
-
-
-// app.listen(port, ()=>{
-//     console.log(`Example app listening on http://localhost:${port}`);
-// });
-
-const e = require('connect-flash');
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const { body, validationResult, check } = require('express-validator');
+const session = require('express-session');
+const cookieParser = require("cookie-parser");
+const flash = require('connect-flash');
 const {Contact} = require('./models');
+const { ResultWithContext } = require('express-validator/src/chain');
+const port = 3000;
+
+
+// Gunakan ejs
+app.set('view engine', 'ejs');
+
+// Third-Party Midleware
+app.use(expressLayouts);
+
+// Built-in Midleware
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+
+// Konfigurasi flash
+
+app.use(cookieParser('secret'));
+app.use(
+    session({
+        cookie: {maxAge: 6000},
+        secret: 'secret',
+        resave: true,
+        saveUninitialized: true,
+    })
+);
+
+app.use(flash());
+
+app.get('/about', (req, res)=>{
+    // res.sendFile('./about.html', {root:__dirname});
+    res.render('about', {
+        layout: 'layouts/main-layouts',
+        title: 'view about'
+    });
+});
+
+app.get('/contact', async (req, res)=>{
+    const contacts = await Contact.findAll();
+    res.render('contact', {
+        layout: 'layouts/main-layouts',
+        title: 'halaman contact',
+        msg: req.flash('msg'),
+        contacts
+    });
+});
+
+// Halaman Tambah Data Contact
+app.get('/contact/add', (req, res)=>{
+    res.render('contact-add',{
+        layout: 'layouts/main-layouts',
+        title: 'Halaman tambah contact'
+    });
+});
+
+// Proses Tambah data contact
+app.post('/contact', [
+    body('nama').custom(async (value)=>{
+        const duplikat = await Contact.findOne({
+            where: {nama: value}
+        });
+        if(duplikat){
+            throw new Error('Nama contact sudah digunakan!');
+        }
+        return true;
+    }),
+    check('email', 'Email tidak valid!').isEmail(), 
+    check('nohp', 'No HP tidak valid!').isMobilePhone('id-ID')
+    ], 
+    (req, res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        // return res.status(400).json({ errors: errors.array() });
+        res.render('contact-add',{
+            layout: 'layouts/main-layouts',
+            title: 'Halaman tambah contact',
+            errors: errors.array()
+        });
+    } else {
+        Contact.create({
+            nama: req.body.nama,
+            nohp: req.body.nohp,
+            email: req.body.email
+        }).then((contact)=>{
+            req.flash('msg', 'Data contact bersail ditambahkan!');
+            res.redirect('/contact');
+        }).catch(err => {
+            res.status(422).json("Can't create article")
+        })
+    }
+}
+);
+
+// halaman detail
+app.get('/contact/:nama', async (req, res)=>{
+    const contact = await Contact.findOne({
+        where: {nama: req.params.nama}
+    });
+    res.render('detail', {
+        layout: 'layouts/main-layouts',
+        title: 'halaman detail contact',
+        contact,
+    });
+});
+
+app.get('/', (req, res)=>{
+    const kariyawan = 
+    [
+        {
+        nama: 'akhmad',
+        nik: '234'
+        },
+        {
+        nama: 'wildan',
+        nik: '234'
+        },
+        {
+        nama: 'arthur',
+        nik: '234'
+        }
+    ]
+    res.render('index', {
+        layout: 'layouts/main-layouts',
+        title: 'Halaman index',
+        kariyawan,
+    });
+});
+
+
+app.listen(port, ()=>{
+    console.log(`Example app listening on http://localhost:${port}`);
+});
+
+
+
 
 // insert psql
 // Contact.create({
@@ -104,7 +167,7 @@ const {Contact} = require('./models');
 // });
 
 // query find at psql
-Contact.findAll().then((contact)=> console.log(contact));
+// Contact.findAll().then((contact)=> console.log(contact));
 // Contact.findOne({
 //     where: {id: 1}
 // })
